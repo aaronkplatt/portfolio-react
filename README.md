318f2768b1269580f0466ff11ea0188d10b7dcdc
Aaron Platt <aaron.k.platt@gmail.com>
2021-04-19 16:39:33 -0700

    portfolio is finished

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ https://docs.google.com/spreadsheets/d/1MwJdYtj5xgPEZlXvEXFNRQ9FxjMbnfvIO6BPseLn
 
 refrence:
 https://github.com/dwyl/learn-to-send-email-via-google-script-html-no-server#how
+
+Grey Friends
+#5EADC2 dark blue
+#334A51 black
+#96B0B7 light blue
+
--- portfolio/src/pages/Portfolio.js
+++ portfolio/src/pages/Portfolio.js
@@ -27,10 +27,10 @@ function Portfolio() {
           <div className="card">
             <h4 className="card-title">Local Coffee Shop Locator</h4>
             <div className="card-image">
-              <img src={LocalCoffeeShop} alt="project"/>
+              <img  className="images" src={LocalCoffeeShop} alt="project"/>
             </div>
             <div className="card-content">
-              <p>Generate locations of nearby coffee shops, relative to a city search, and the current weather conditions for the city.</p>
+              <p>Generate locations of nearby local coffee shops and the current weather conditions for the city. Because we are displaying local coffee shops we must exclude bug brand coffee shops like starbucks.</p>
               <hr></hr>
               <p>Tools/Languages: HTML5, CSS, Javascript, Bootstrap</p>
             </div>
@@ -47,12 +47,12 @@ function Portfolio() {
           <div className="card">
             <h4 className="card-title">Cobra Arcade</h4>
             <div className="card-image">
-              <img src={CobraArcade} alt="project"/>
+              <img className="images" src={CobraArcade} alt="project"/>
             </div>
             <div className="card-content">
-              <p>Enjoy a nostalgic trip down game history to play classic titles such as Snake, Flappy Bird, and Pong.</p>
+              <p>Enjoy a nostalgic trip down game history to play classic titles such as Snake, Flappy Bird, and Pong. User data is saved for highscores for each game.</p>
               <hr></hr>
-              <p>Tools/Languages: Sequelize, Node.JS, Express, Express Handlebars, Express Sessions, Canvas</p>
+              <p>Tools/Languages: Sequelize, Node.JS, Express, Express Handlebars, Express Sessions, Canvas, Iframe</p>
             </div>
             <div className="card-action">
               <a href="https://cobra-arcade.herokuapp.com/" className="links">Deployed</a>
@@ -67,10 +67,10 @@ function Portfolio() {
           <div className="card">
             <h4 className="card-title">EAT-DA-BURGER</h4>
             <div className="card-image">
-              <img src={Burger} alt="project"/>
+              <img  className="images" src={Burger} alt="project"/>
             </div>
             <div className="card-content">
-              <p>This program uses MySql Database to create burgers. It allows you to devour the burger, which is saved into the devour table.
+              <p>This program uses MySql Database to create burgers. It allows you to devour the burger by clicking th edvour button, after its saved into the devour table. 
               </p>
               <hr />
               <p>Tools/Languages: HTML5, CSS, Javascript, MySQL, Express.js, Express Handlebars</p>
@@ -88,7 +88,7 @@ function Portfolio() {
           <div className="card">
             <h4 className="card-title">OFFLINE/ONLINE BUDGET TRACKER</h4>
             <div className="card-image">
-              <img src={Budget} alt="project"/>
+              <img  className="images" src={Budget} alt="project"/>
             </div>
             <div className="card-content">
               <p>As an avid traveller I want to be able to track my withdrawals and deposits with or without a data/internet connection so that my account balance is accurate when I am traveling.</p>
@@ -111,12 +111,12 @@ function Portfolio() {
             <div className="card">
               <h4 className="card-title">NOMAD WILDERNESS</h4>
               <div className="card-image">
-                <img src={Nomad} alt="project"/>
+                <img  className="images" src={Nomad} alt="project"/>
               </div>
               <div className="card-content">
                 <p>Nomad will assist users in finding edible plants, nearby animals and provide several survival guides.</p>
                 <hr />
-                <p>Tools/Languages: ReactJS, Node and Express Web Server, MongoDB Database with a Mongoose ORM, GET and POST routes, Deployed using Heroku, GoogleMaps API, Auth0, React Materialize CSS, Passport, Axios</p>
+                <p>Tools/Languages: ReactJS, Node and Express Web Server, MongoDB, Mongoose ORM, GoogleMaps API, Auth0, React Materialize CSS, Passport, Axios</p>
               </div>
               <div className="card-action">
                 <a href="https://wilderness-survival-app.herokuapp.com/" className="links">Deployed</a>
@@ -134,7 +134,7 @@ function Portfolio() {
                 <img className="images" src={Tracker} alt="project"/>
               </div>
               <div className="card-content">
-                <p>As a buisness owner I want to be able to view and manage the departments, roles, and employees in my company so that I can organize and plan my business</p>
+                <p>As a buisness owner I want to be able to view and manage the Departments, roles, Managers and employees in my company so that I can organize, view, and plan buisness accordingly.</p>
                 <hr />
                 <p>Tools/Languages: MySQL, Express, Inquirer, Javascript, Console Table</p>
               </div>
@@ -154,7 +154,7 @@ function Portfolio() {
                 <img className="images" src={Team} alt="project"/>
               </div>
               <div className="card-content">
-                <p>This project asks the user to enter information about each Employee and creates an HTML file in the folder "output" for the team putting each employee on its own card.</p>
+                <p>This project asks the user to enter information about each Employee and creates an HTML file in the folder "output" for the team putting each employee on its own card and displaying their info.</p>
                 <hr />
                 <p>Tools/Languages: Html5, Inquirer, Javascript</p>
               </div>
@@ -171,10 +171,11 @@ function Portfolio() {
             <div className="card">
               <h4 className="card-title">SUNSHINE SURFCLUB</h4>
               <div className="card-image">
-                <img src={Surf} alt="project"/>
+                <img  className="images" src={Surf} alt="project"/>
               </div>
               <div className="card-content">
-                <p>This single page application allows people to sign up for a Surf Club. All of the form responses are saved onto a Google Sheets.</p>
+                <p>This single page application allows people to sign up for the Sunshine Surf Club. All of the form responses are saved onto a private Google Sheets for the client to pull data from.</p>
+                <hr />
                 <p>Tools/Languages: HTML5, Javascript, CSS, Google Sheets</p>
               </div>
               <div className="card-action">
@@ -184,10 +185,9 @@ function Portfolio() {
               </div>
             </div>
           </div>
-          
         </div>
       </div>
-    </div>
+      </div>
   );
 }
 
