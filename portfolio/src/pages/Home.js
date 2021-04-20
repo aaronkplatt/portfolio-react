774b3a00a2b7a91ed0ea579eb4b09768d9e654fb
Aaron Platt <aaron.k.platt@gmail.com>
2021-04-20 14:44:58 -0700

    I added the pdk resume and also am done with the home page

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -2,44 +2,51 @@ import React from "react";
 import 'materialize-css';
 import "./style/Home.css";
 import profilePic from "../pages/images/ProfilePic.jpg";
+import resume from './documents/Aaron_Platt_Final_RESUME.pdf';
 
 
 function Home() {
   return (
-    <div className='row'>
-      <h1>I'm Aaron Platt</h1>
-      <div className="col s4 card">
-        {/* SKILLS */}
-        <h3>SKILLS</h3>
-            <ul>
-              <li>HTML</li>
-              <li>CSS</li>
-              <li>Javascript/jQuery</li>
-              <li>Ajax</li>
-              <li>Vue.js</li>
-              <li>REACTJS</li>
-              <li>NODEJS</li>
-              <li>SQL</li>
-              <li>MONGODB</li>
-              <li>Express Web Server</li>
-              <li>FULLSTACK</li>
-            </ul>
-      </div>
-      {/* HEADER */}
-      <div className='col s4'>
-      {/* PROFILE PIC */}
-        <img src={profilePic} alt="Profile Pic" className="profilePic"></img> 
-      </div>
+    <div className="container-fluid">
+      <div className='row'>
+        <h1 className="name">I'm Aaron Platt.</h1>
+        <div className="col s3 offset-s1 card outsideCard">
+          {/* SKILLS */}
+          <h3 className="cardHead">Skills</h3>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript/jQuery</li>
+            <li>Bootstrap/Materialize</li>
+            <li>ReactJS</li>
+            <li>NodeJS</li>
+            <li>Ajax</li>
+            <li>SQL</li>
+            <li>MongoDB</li>
+            <li>IndexedDB</li>
+            <li>MomentJS</li>
+            <li>VueJS</li>
+            <li>Express Web Server</li>
+            <li>Google Sheets</li>
+        </div>
+        {/* HEADER */}
+        <div className='col s4'>
+        {/* PROFILE PIC */}
+          <img src={profilePic} alt="Profile Pic" className="profilePic"></img> 
+          <h3 className="links">Links</h3>
+        </div>
 
-      {/* ABOUT ME */}
-      <div className="col s4 card">
-        <h3>About Me</h3>
-        <p className="bio">
-          Logical and results-driven Web Developer dedicated to building and optimizing user-focused websites for customers with various business objectives. Judicious and creative when crafting effective websites, apps and platforms to propel competitive advantage and revenue growth. Technically proficient and analytical problem solver with calm and focused demeanor.
-        </p>
-        <button className="personalLink" href="https://drive.google.com/file/d/1DEONHnCfec0uyCn1ZC9pRInYITlsTfqk/view?usp=sharing">RESUME</button>
-          <button className="personalLink" href="https://github.com/aaronkplatt">GITHUB</button>
-          <button className="personalLink" href="https://www.linkedin.com/in/aaron-platt-90808116a/">LINKEDIN</button>
+        {/* ABOUT ME */}
+        <div className="col s3 card outsideCard">
+          <h3 className="cardHead">About Me</h3>
+          <p className="bio">
+            Logical and results-driven Web Developer dedicated to building and optimizing user-focused websites for customers with various business objectives. Judicious and creative when crafting effective websites, apps and platforms to propel competitive advantage and revenue growth. Technically proficient and analytical problem solver with calm and focused demeanor.
+          </p>
+        </div>
+      </div>
+      <div className="row">
+        <a className="personalLink" href={resume}>RESUME</a>
+        <a className="personalLink" href="https://www.linkedin.com/in/aaron-platt-90808116a/">LINKEDIN</a>
+        <a className="personalLink" href="https://github.com/aaronkplatt">GITHUB</a>
       </div>
     </div>
   );
